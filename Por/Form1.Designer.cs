f033cf7ff6538b81484accd452805d39d2d3f81e
khoksung <xpepex00701@hotmail.com>
2013-11-17 02:00:13 +0700

    Project
    Project

---
+++ Por/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Por
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trbBlue = new System.Windows.Forms.TrackBar();
+            this.trbGreen = new System.Windows.Forms.TrackBar();
+            this.trbRed = new System.Windows.Forms.TrackBar();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trbBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbRed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.ForeColor = System.Drawing.Color.Yellow;
+            this.txtResult.Location = new System.Drawing.Point(226, 296);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(153, 54);
+            this.txtResult.TabIndex = 15;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(84, 312);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "สีที่ผสมแล้ว";
+            // 
+            // trbBlue
+            // 
+            this.trbBlue.Location = new System.Drawing.Point(127, 235);
+            this.trbBlue.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.trbBlue.Maximum = 255;
+            this.trbBlue.Name = "trbBlue";
+            this.trbBlue.Size = new System.Drawing.Size(393, 45);
+            this.trbBlue.TabIndex = 13;
+            this.trbBlue.TickFrequency = 5;
+            this.trbBlue.Scroll += new System.EventHandler(this.trbBlue_Scroll);
+            // 
+            // trbGreen
+            // 
+            this.trbGreen.Location = new System.Drawing.Point(127, 173);
+            this.trbGreen.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.trbGreen.Maximum = 255;
+            this.trbGreen.Name = "trbGreen";
+            this.trbGreen.Size = new System.Drawing.Size(393, 45);
+            this.trbGreen.TabIndex = 12;
+            this.trbGreen.TickFrequency = 5;
+            this.trbGreen.Scroll += new System.EventHandler(this.trbGreen_Scroll);
+            // 
+            // trbRed
+            // 
+            this.trbRed.Location = new System.Drawing.Point(127, 110);
+            this.trbRed.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.trbRed.Maximum = 255;
+            this.trbRed.Name = "trbRed";
+            this.trbRed.Size = new System.Drawing.Size(393, 45);
+            this.trbRed.TabIndex = 11;
+            this.trbRed.TickFrequency = 5;
+            this.trbRed.Scroll += new System.EventHandler(this.trbRed_Scroll);
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblBlue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblBlue.Location = new System.Drawing.Point(35, 235);
+            this.lblBlue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(81, 27);
+            this.lblBlue.TabIndex = 10;
+            this.lblBlue.Text = "สีน้ำเงิน";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblGreen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblGreen.Location = new System.Drawing.Point(45, 173);
+            this.lblGreen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(69, 27);
+            this.lblGreen.TabIndex = 9;
+            this.lblGreen.Text = "สีเขียว";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblRed.ForeColor = System.Drawing.Color.Red;
+            this.lblRed.Location = new System.Drawing.Point(50, 110);
+            this.lblRed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(62, 27);
+            this.lblRed.TabIndex = 8;
+            this.lblRed.Text = "สีแดง";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "ณัฐชา หงษ์ทอง 5533266105 16.3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(206, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "การผสมสี";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 460);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trbBlue);
+            this.Controls.Add(this.trbGreen);
+            this.Controls.Add(this.trbRed);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.trbBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbRed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trbBlue;
+        private System.Windows.Forms.TrackBar trbGreen;
+        private System.Windows.Forms.TrackBar trbRed;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Por/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Por
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trbRed_Scroll(object sender, EventArgs e)
+        {
+            lblRed.Text = "สีแดง = " + trbRed.Value.ToString();
+            lblRed.BackColor = Color.FromArgb(trbRed.Value, 0, 0);
+            UpdateColor();
+        }
+
+        private void trbGreen_Scroll(object sender, EventArgs e)
+        {
+            lblGreen.Text = "สีเขียว = " + trbGreen.Value.ToString();
+            lblGreen.BackColor = Color.FromArgb(0, trbGreen.Value, 0);
+            UpdateColor();
+        }
+
+        private void trbBlue_Scroll(object sender, EventArgs e)
+        {
+            lblBlue.Text = "สีน้ำเงิน = " + trbBlue.Value.ToString();
+            lblBlue.BackColor = Color.FromArgb(0, 0, trbBlue.Value);
+            UpdateColor();
+        }
+
+        private void UpdateColor()
+        {
+            txtResult.BackColor = Color.FromArgb(trbRed.Value, trbGreen.Value, trbBlue.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRed.ForeColor = Color.White;
+            lblGreen.ForeColor = Color.White;
+            lblBlue.ForeColor = Color.White;
+            UpdateColor();
+        }
+
+        private void lblRed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGreen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblBlue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
